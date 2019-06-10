16f3dc7d3dd698e00109cb88aac7a8a53948a5ae
Aaron Chu <aaronchu357@gmail.com>
2019-06-10 16:00:30 -0400

    Stuff

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,30 @@
-const BASE_URL = "http://localhost:3000"
-const TRAINERS_URL = `${BASE_URL}/trainers`
-const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener("DOMContentLoaded", () => {
+
+  const BASE_URL = "http://localhost:3000"
+  const TRAINERS_URL = `${BASE_URL}/trainers`
+  const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+  const mainTag = document.querySelector("main")
+  fetch(TRAINERS_URL)
+  .then(resp => resp.json())
+  .then(trainers => {
+    trainers.forEach(trainer => {
+      mainTag.innerHTML += `
+        <div class="card" data-id="${trainer.id}">
+          <p>${trainer.name}</p>
+          <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+          <ul>
+          </ul>
+        </div>
+      `
+  })
+})
+
+  const pokemonLi = `
+    <li>
+      ${pokemon.nickname} (${pokemon.species}) <button class="release" data-pokemon-id="${pokemon.id}">Release</button>
+    </li>
+  `
+
+
+})
